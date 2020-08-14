90b59e06a9094bb54091ca74154a218d75aedb3f
Raxit Majithiya <rax@expa.com>
2020-08-13 23:15:46 -0400

    Add package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "BlowinSwiper",
+  products: [
+    // Products define the executables and libraries produced by a package, and make them visible to other packages.
+    .library(
+      name: "BlowinSwiper",
+      targets: ["BlowinSwiper"]
+    ),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+    .target(
+      name: "BlowinSwiper",
+      dependencies: [],
+      path: "BlowinSwiper"
+    ),
+  ]
+)
